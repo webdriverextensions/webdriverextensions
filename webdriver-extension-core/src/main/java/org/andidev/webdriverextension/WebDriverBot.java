35c4302967a3e77ec37673ff59e128467a9ff795
Anders Steiner <anders.steiner@gmail.com>
2013-01-19 11:12:16 +0100

    Intriduced alphabetical orderding and minor changes

--- webdriver-extension-core/src/main/java/org/andidev/webdriverextension/WebDriverBot.java
+++ webdriver-extension-core/src/main/java/org/andidev/webdriverextension/WebDriverBot.java
@@ -1,7 +1,6 @@
 package org.andidev.webdriverextension;
 
 import java.util.List;
-import mx4j.tools.remote.http.WebContainer;
 import org.apache.commons.lang3.math.NumberUtils;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.NoSuchElementException;
@@ -16,31 +15,14 @@ public class WebDriverBot {
     public static double delayTime = 0.0;
     // public static boolean debugMode = false; Implement later
 
-    public static void click(WebElement webElement) {
+    public static void check(WebElement webElement) {
         delay();
-        webElement.click();
-        // DEBUG TEXT: Clicked <tagname ...>
-    }
-
-    public static void type(String str, WebElement webElement) {
-        if (str == null) {
-            return;
+        if (!isChecked(webElement)) {
+            webElement.click();
+            // DEBUG TEXT: Checked <input type="checkbox" name="">
+        } else {
+            // DEBUG TEXT: Checkbox already checked! Skipped checking <input type="checkbox" name="">           
         }
-        delay();
-        webElement.sendKeys(str);
-        // DEBUG TEXT: Typed "str" in <input name="">
-    }
-
-    public static void pressKeys(WebElement webElement, CharSequence... keysToSend) {
-        delay();
-        webElement.sendKeys(keysToSend);
-        // DEBUG TEXT: Pressed keys "keysToSend" in <input name="">
-    }
-
-    public static void pressEnter(WebElement webElement) {
-        delay();
-        webElement.sendKeys(Keys.ENTER);
-        // DEBUG TEXT: Pressed enter key in <input name="">
     }
 
     public static void clear(WebElement webElement) {
@@ -57,29 +39,15 @@ public class WebDriverBot {
         type(str, webElement);
     }
 
+    public static void click(WebElement webElement) {
+        delay();
+        webElement.click();
+        // DEBUG TEXT: Clicked <tagname ...>
+    }
+
     public static boolean isChecked(WebElement webElement) {
         delay();
         return webElement.isSelected();
-    }
-
-    public static boolean isText(String expected, WebElement webElement) {
-        delay();
-        return webElement.getText().equals(expected);
-    }
-
-    public static boolean isTextContaining(String expected, WebElement webElement) {
-        delay();
-        return webElement.getText().contains(expected);
-    }
-    
-    public static boolean isTextStartingWith(String expected, WebElement webElement) {
-        delay();
-        return webElement.getText().startsWith(expected);
-    }
-    
-    public static boolean isTextEndingWith(String expected, WebElement webElement) {
-        delay();
-        return webElement.getText().endsWith(expected);
     }
 
     public static boolean isDisplayed(WebElement webElement) {
@@ -104,24 +72,50 @@ public class WebDriverBot {
         return isLoaded;
     }
 
-    public static void check(WebElement webElement) {
+    public static boolean isText(String expected, WebElement webElement) {
         delay();
-        if (!isChecked(webElement)) {
-            webElement.click();
-            // DEBUG TEXT: Checked <input type="checkbox" name="">
-        } else {
-            // DEBUG TEXT: Checkbox already checked! Skipped checking <input type="checkbox" name="">           
-        }
+        return webElement.getText().equals(expected);
     }
 
-    public static void uncheck(WebElement webElement) {
+    public static boolean isTextContaining(String expected, WebElement webElement) {
         delay();
-        if (isChecked(webElement)) {
-            webElement.click();
-            // DEBUG TEXT: Un-checked <input type="checkbox" name="">
-        } else {
-            // DEBUG TEXT: Checkbox already un-checked! Skipped un-checking <input type="checkbox" name="">           
+        return webElement.getText().contains(expected);
+    }
+
+    public static boolean isTextEndingWith(String expected, WebElement webElement) {
+        delay();
+        return webElement.getText().endsWith(expected);
+    }
+
+    public static boolean isTextStartingWith(String expected, WebElement webElement) {
+        delay();
+        return webElement.getText().startsWith(expected);
+    }
+
+    public static void open(WebPage page) {
+        delay();
+        page.get();
+    }
+
+    public static void pressKeys(WebElement webElement, CharSequence... keysToSend) {
+        delay();
+        webElement.sendKeys(keysToSend);
+        // DEBUG TEXT: Pressed keys "keysToSend" in <input name="">
+    }
+
+    public static void pressEnter(WebElement webElement) {
+        delay();
+        webElement.sendKeys(Keys.ENTER);
+        // DEBUG TEXT: Pressed enter key in <input name="">
+    }
+
+    public static void type(String str, WebElement webElement) {
+        if (str == null) {
+            return;
         }
+        delay();
+        webElement.sendKeys(str);
+        // DEBUG TEXT: Typed "str" in <input name="">
     }
 
     public static String read(WebElement webElement) {
@@ -132,7 +126,7 @@ public class WebDriverBot {
     public static Double readAsNumber(WebElement webElement) {
         delay();
         String string = read(webElement);
-        if(NumberUtils.isNumber(string)) {
+        if (NumberUtils.isNumber(string)) {
             return NumberUtils.toDouble(string);
         } else {
             return null;
@@ -145,30 +139,35 @@ public class WebDriverBot {
         // DEBUG TEXT: Selected "optionText" from <select name="">
     }
 
-    public static void open(WebPage page) {
+    public static void uncheck(WebElement webElement) {
         delay();
-        page.get();
+        if (isChecked(webElement)) {
+            webElement.click();
+            // DEBUG TEXT: Un-checked <input type="checkbox" name="">
+        } else {
+            // DEBUG TEXT: Checkbox already un-checked! Skipped un-checking <input type="checkbox" name="">           
+        }
     }
 
+    private static void debug(String str) {
+        System.out.println(str);
+    }
 
     public static void debugNumberOfElements(List<WebElement> webElements) {
-        delay();
         System.out.println("Number of " + webElements.get(0).getTagName() + "-tags: " + webElements.size());
     }
 
     public static void debugText(WebElement webElement) {
-        delay();
         System.out.println("Text in " + webElement.getTagName() + "-tag: " + webElement.getText());
     }
 
     public static void debugText(List<WebElement> webElements) {
-        delay();
         for (WebElement webElement : webElements) {
             debugText(webElement);
         }
     }
 
-    public static void waitForVisibilityOfElement(WebElement webElement, WebDriver driver) {
+    public static void waitForHtmlToDisplay(WebElement webElement, WebDriver driver) {
         delay();
         WebDriverWait wait = new WebDriverWait(driver, 30);
         wait.until(ExpectedConditions.visibilityOf(webElement));
@@ -188,9 +187,5 @@ public class WebDriverBot {
 
     public static void delay() {
         delay(delayTime);
-    }
-
-    private static void debug(String str) {
-        System.out.println(str);
     }
 }
