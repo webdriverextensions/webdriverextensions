981a25b14e66b356177a1a4aef4abda5068b21f6
Anders Steiner <anders.steiner@gmail.com>
2013-01-27 11:22:52 +0100

    Removed unused import

--- webdriver-extension-core/src/main/java/org/andidev/webdriverextension/WebDriverBot.java
+++ webdriver-extension-core/src/main/java/org/andidev/webdriverextension/WebDriverBot.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.LoadableComponent;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
