4fa33d29f7f85c12e3632c3f577dbc8bc85cb6b4
Anders Steiner <anders.steiner@gmail.com>
2013-01-30 13:48:13 +0100

    Changed version convention and removed incorrect dependency

--- webdriver-extension-example-google/src/test/java/org/andidev/webdriverextension/examples/google/SearchPageTest.java
+++ webdriver-extension-example-google/src/test/java/org/andidev/webdriverextension/examples/google/SearchPageTest.java
@@ -6,12 +6,11 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.andidev.webdriverextension.site.WebDriverExtensionSite;
 
 public class SearchPageTest extends PageAware {
 
     public SearchPageTest() {
-//        setDriver(new FirefoxDriver());
+        setDriver(new FirefoxDriver());
     }
 
     @Before
@@ -24,19 +23,12 @@ public class SearchPageTest extends PageAware {
 
     @Test
     public void searchTest() {
-//        getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-//        getDriver().manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
-//        open(searchPage);
-//        type("Test", searchPage.searchQuery);
-//        delay(1);
-////        click(searchPage.googleSearch);
-//        getDriver().close();
-
-        WebDriverExtensionSite site = new WebDriverExtensionSite();
-        site.setDriver(new FirefoxDriver());
-        open(site.examplesPage.site);
-        site.examplesPage.assertIsOpen();
-        open(site.examplesPage.site.examplesPage);
-        site.getDriver().close();
+        getDriver().manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        getDriver().manage().timeouts().pageLoadTimeout(5, TimeUnit.SECONDS);
+        open(searchPage);
+        type("Test", searchPage.searchQuery);
+        delay(1);
+//        click(searchPage.googleSearch);
+        getDriver().close();
     }
 }
