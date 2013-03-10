680a4dab232d2d25f9eb2c2a12431d860610cfe5
Anders Steiner <anders.steiner@gmail.com>
2013-03-10 15:54:57 +0100

    Removed festbot

--- webdriver-extension-core/src/main/java/org/andidev/webdriverextension/bot/fest/asserttypes/AssertClassTypes.java
+++
@@ -1,9 +0,0 @@
-package org.andidev.webdriverextension.bot.fest.asserttypes;
-
-public class AssertClassTypes {
-
-    public AssertClassTypes(String attribute) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-}
