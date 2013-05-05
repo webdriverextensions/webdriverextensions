309a339c56ec0012bebb94d71e6a522210fd1dd0
anders <anders@192.168.43.31>
2013-05-05 08:13:06 +0200

    Renamed Environment to CurrentBrowser

---
+++ webdriver-extension-core/src/main/java/org/andidev/webdriverextension/area51/CurrentBrowser.java
@@ -0,0 +1,68 @@
+package org.andidev.webdriverextension.area51;
+
+import java.net.URL;
+import org.andidev.webdriverextension.exceptions.WebDriverExtensionException;
+import org.openqa.selenium.Platform;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class CurrentBrowser {
+
+    private static ThreadLocal<WebDriver> driverTl = new ThreadLocal<WebDriver>();
+
+    public static String getBrowserName() {
+        return ((RemoteWebDriver) getDriver()).getCapabilities().getBrowserName();
+    }
+
+    public static String getBrowserVersion() {
+        return ((RemoteWebDriver) getDriver()).getCapabilities().getVersion();
+    }
+
+    public static Platform getPlatform() {
+        return ((RemoteWebDriver) getDriver()).getCapabilities().getPlatform();
+    }
+
+    public static WebDriver getDriver() {
+        if (driverTl.get() == null) {
+            throw new WebDriverExtensionException("Driver in CurrentBrowser is not set. Please set the driver with any CurrentBrowser.setDriver(...) method before using it. Note that the driver will be thread safe since ThreadLocal is used so don't worry about thread safety.");
+        }
+        return driverTl.get();
+    }
+
+    public static void setDriver(WebDriver driver) {
+        driverTl.set(driver);
+    }
+
+    public static void setDriver(DesiredCapabilities desiredCapabilities) {
+        driverTl.set(new RemoteWebDriver(desiredCapabilities));
+    }
+
+    public static void setDriver(URL remoteAddress, DesiredCapabilities desiredCapabilities) {
+        driverTl.set(new RemoteWebDriver(remoteAddress, desiredCapabilities));
+    }
+
+    public static void setDriver(String browserName) {
+        driverTl.set(new RemoteWebDriver(new DesiredCapabilities(browserName, "", Platform.ANY)));
+    }
+
+    public static void setDriver(URL remoteAddress, String browserName) {
+        driverTl.set(new RemoteWebDriver(remoteAddress, new DesiredCapabilities(browserName, "", Platform.ANY)));
+    }
+
+    public static void setDriver(String browserName, String browserVersion) {
+        driverTl.set(new RemoteWebDriver(new DesiredCapabilities(browserName, browserVersion, Platform.ANY)));
+    }
+
+    public static void setDriver(URL remoteAddress, String browserName, String browserVersion) {
+        driverTl.set(new RemoteWebDriver(remoteAddress, new DesiredCapabilities(browserName, browserVersion, Platform.ANY)));
+    }
+
+    public static void setDriver(String browserName, String browserVersion, Platform platform) {
+        driverTl.set(new RemoteWebDriver(new DesiredCapabilities(browserName, browserVersion, platform)));
+    }
+
+    public static void setDriver(URL remoteAddress, String browserName, String browserVersion, Platform platform) {
+        driverTl.set(new RemoteWebDriver(remoteAddress, new DesiredCapabilities(browserName, browserVersion, platform)));
+    }
+}
