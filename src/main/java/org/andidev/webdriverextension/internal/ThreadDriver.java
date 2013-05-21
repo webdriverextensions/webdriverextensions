550bb29d969f2b4f7f98aa4d4d1605c3647a7fbc
Anders Steiner <anders.steiner@gmail.com>
2013-05-21 06:34:47 +0200

    Added remove method to ThreadDriver

--- src/main/java/org/andidev/webdriverextension/internal/ThreadDriver.java
+++ src/main/java/org/andidev/webdriverextension/internal/ThreadDriver.java
@@ -8,48 +8,52 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class ThreadDriver {
 
-    private static ThreadLocal<WebDriver> driverTl = new ThreadLocal<WebDriver>();
+    private static ThreadLocal<WebDriver> threadDriver = new ThreadLocal<WebDriver>();
 
     public static WebDriver getDriver() {
-        if (driverTl.get() == null) {
+        if (threadDriver.get() == null) {
             throw new WebDriverExtensionException("Driver in CurrentBrowser is not set. Please set the driver with any CurrentBrowser.setDriver(...) method before using it. Note that the driver will be thread safe since ThreadLocal is used so don't worry about thread safety.");
         }
-        return driverTl.get();
+        return threadDriver.get();
     }
 
     public static void setDriver(WebDriver driver) {
-        driverTl.set(driver);
+        threadDriver.set(driver);
+    }
+
+    public static void removeDriver() {
+        threadDriver.remove();
     }
 
     public static void setDriver(DesiredCapabilities desiredCapabilities) {
-        driverTl.set(new RemoteWebDriver(desiredCapabilities));
+        threadDriver.set(new RemoteWebDriver(desiredCapabilities));
     }
 
     public static void setDriver(URL remoteAddress, DesiredCapabilities desiredCapabilities) {
-        driverTl.set(new RemoteWebDriver(remoteAddress, desiredCapabilities));
+        threadDriver.set(new RemoteWebDriver(remoteAddress, desiredCapabilities));
     }
 
     public static void setDriver(String browserName) {
-        driverTl.set(new RemoteWebDriver(new DesiredCapabilities(browserName, "", Platform.ANY)));
+        threadDriver.set(new RemoteWebDriver(new DesiredCapabilities(browserName, "", Platform.ANY)));
     }
 
     public static void setDriver(URL remoteAddress, String browserName) {
-        driverTl.set(new RemoteWebDriver(remoteAddress, new DesiredCapabilities(browserName, "", Platform.ANY)));
+        threadDriver.set(new RemoteWebDriver(remoteAddress, new DesiredCapabilities(browserName, "", Platform.ANY)));
     }
 
     public static void setDriver(String browserName, String browserVersion) {
-        driverTl.set(new RemoteWebDriver(new DesiredCapabilities(browserName, browserVersion, Platform.ANY)));
+        threadDriver.set(new RemoteWebDriver(new DesiredCapabilities(browserName, browserVersion, Platform.ANY)));
     }
 
     public static void setDriver(URL remoteAddress, String browserName, String browserVersion) {
-        driverTl.set(new RemoteWebDriver(remoteAddress, new DesiredCapabilities(browserName, browserVersion, Platform.ANY)));
+        threadDriver.set(new RemoteWebDriver(remoteAddress, new DesiredCapabilities(browserName, browserVersion, Platform.ANY)));
     }
 
     public static void setDriver(String browserName, String browserVersion, Platform platform) {
-        driverTl.set(new RemoteWebDriver(new DesiredCapabilities(browserName, browserVersion, platform)));
+        threadDriver.set(new RemoteWebDriver(new DesiredCapabilities(browserName, browserVersion, platform)));
     }
 
     public static void setDriver(URL remoteAddress, String browserName, String browserVersion, Platform platform) {
-        driverTl.set(new RemoteWebDriver(remoteAddress, new DesiredCapabilities(browserName, browserVersion, platform)));
+        threadDriver.set(new RemoteWebDriver(remoteAddress, new DesiredCapabilities(browserName, browserVersion, platform)));
     }
 }
