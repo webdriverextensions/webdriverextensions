c2ee009edfd5f4922a2a2f806eae1a2ebbd9f92e
Anders Steiner <anders.steiner@gmail.com>
2014-04-19 20:46:49 +0200

    Added InstanceUtils

---
+++ src/main/java/com/github/webdriverextensions/internal/utils/InstanceUtils.java
@@ -0,0 +1,26 @@
+package com.github.webdriverextensions.internal.utils;
+
+import com.github.webdriverextensions.internal.WebDriverExtensionException;
+import java.lang.reflect.InvocationTargetException;
+
+public class InstanceUtils {
+
+    public static <T> T newInstance(Class clazz, Class<T> returnedClazz) {
+        try {
+            return (T) clazz.getConstructor().newInstance();
+        } catch (NoSuchMethodException ex) {
+            throw new WebDriverExtensionException(ex);
+        } catch (SecurityException ex) {
+            throw new WebDriverExtensionException(ex);
+        } catch (InstantiationException ex) {
+            throw new WebDriverExtensionException(ex);
+        } catch (IllegalAccessException ex) {
+            throw new WebDriverExtensionException(ex);
+        } catch (IllegalArgumentException ex) {
+            throw new WebDriverExtensionException(ex);
+        } catch (InvocationTargetException ex) {
+            throw new WebDriverExtensionException(ex);
+        }
+    }
+
+}
