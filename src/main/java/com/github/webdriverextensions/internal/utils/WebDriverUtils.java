909fb928061c327dd945fad20ad8fc596f727ae3
Anders Steiner <anders.steiner@gmail.com>
2014-04-21 12:05:29 +0200

    Added removeCapabilities to WebDriverUtils

--- src/main/java/com/github/webdriverextensions/internal/utils/WebDriverUtils.java
+++ src/main/java/com/github/webdriverextensions/internal/utils/WebDriverUtils.java
@@ -1,8 +1,13 @@
 package com.github.webdriverextensions.internal.utils;
 
+import com.google.common.base.Predicate;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Sets;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import java.util.Set;
 import org.openqa.selenium.Capabilities;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class WebDriverUtils {
 
@@ -11,4 +16,19 @@ public class WebDriverUtils {
         return gson.toJson(capabilities.asMap());
     }
 
+    public static Capabilities removeCapabilities(Capabilities capabilities, String... keysToRemove) {
+        if (capabilities == null) {
+            return new DesiredCapabilities();
+        }
+
+        final Set<String> keysToRemoveSet = Sets.newHashSet(keysToRemove);
+        capabilities = new DesiredCapabilities(Maps.filterKeys(capabilities.asMap(), new Predicate<String>() {
+            @Override
+            public boolean apply(String key) {
+                return !keysToRemoveSet.contains(key);
+            }
+        }));
+
+        return capabilities;
+    }
 }
