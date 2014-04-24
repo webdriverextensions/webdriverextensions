485820c9e9cda336daed014fa4280879ef653cce
Anders Steiner <anders.steiner@gmail.com>
2014-04-24 21:04:13 +0200

    Refactored ExperimentalBot to cast driver to HasCapabilities interface

--- src/main/java/com/github/webdriverextensions/internal/ExperimentalBot.java
+++ src/main/java/com/github/webdriverextensions/internal/ExperimentalBot.java
@@ -2,142 +2,71 @@ package com.github.webdriverextensions.internal;
 
 import com.github.webdriverextensions.ThreadDriver;
 import org.apache.commons.lang3.StringUtils;
+import org.openqa.selenium.HasCapabilities;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Platform;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.remote.BrowserType;
-import org.openqa.selenium.remote.RemoteWebDriver;
-import org.openqa.selenium.safari.SafariDriver;
 
 public class ExperimentalBot {
 
     /* Browser */
     public static String browser() {
-        if (ThreadDriver.getDriver() instanceof RemoteWebDriver) {
-            return ((RemoteWebDriver) ThreadDriver.getDriver()).getCapabilities().getBrowserName();
-        }
-        throw new WebDriverExtensionException("Sorry! browser() is only "
-                + "implemented for RemoteWebDriver at the moment.");
+        return ((HasCapabilities) ThreadDriver.getDriver()).getCapabilities().getBrowserName();
     }
 
     public static boolean browserIs(String browserName) {
-        if (ThreadDriver.getDriver() instanceof RemoteWebDriver) {
-            return isRemoteWebDriverBrowser(browserName);
-        }
-        throw new WebDriverExtensionException("Sorry! browserIs(String browserType) is only "
-                + "implemented for RemoteWebDriver at the moment.");
+        String currentBrowserName = ((HasCapabilities) ThreadDriver.getDriver()).getCapabilities().getBrowserName();
+        return StringUtils.equalsIgnoreCase(currentBrowserName, browserName);
     }
 
     public static boolean browserIsAndroid() {
-        if (ThreadDriver.getDriver() instanceof RemoteWebDriver) {
-            return isRemoteWebDriverBrowser(BrowserType.ANDROID);
-        }
-        throw new WebDriverExtensionException("Sorry! browserIsAndroid() is only "
-                + "implemented for RemoteWebDriver at the moment.");
+        return browserIs(BrowserType.ANDROID);
     }
 
     public static boolean browserIsChrome() {
-        if (ThreadDriver.getDriver() instanceof RemoteWebDriver) {
-            return isRemoteWebDriverBrowser(BrowserType.CHROME);
-        } else if (ThreadDriver.getDriver() instanceof ChromeDriver) {
-            return true;
-        }
-        return false;
+        return browserIs(BrowserType.CHROME) || browserIs(BrowserType.GOOGLECHROME);
     }
 
     public static boolean browserIsFirefox() {
-        if (ThreadDriver.getDriver() instanceof RemoteWebDriver) {
-            return isRemoteWebDriverBrowser(BrowserType.FIREFOX);
-        } else if (ThreadDriver.getDriver() instanceof FirefoxDriver) {
-            return true;
-        }
-        return false;
+        return browserIs(BrowserType.FIREFOX);
     }
 
     public static boolean browserIsHtmlUnit() {
-        if (ThreadDriver.getDriver() instanceof RemoteWebDriver) {
-            return isRemoteWebDriverBrowser(BrowserType.HTMLUNIT);
-        } else if (ThreadDriver.getDriver() instanceof HtmlUnitDriver) {
-            return true;
-        }
-        return false;
+        return browserIs(BrowserType.HTMLUNIT);
     }
 
     public static boolean browserIsIPad() {
-        if (ThreadDriver.getDriver() instanceof RemoteWebDriver) {
-            return isRemoteWebDriverBrowser(BrowserType.IPAD);
-        }
-        throw new WebDriverExtensionException("Sorry! browserIsIPad() is only "
-                + "implemented for RemoteWebDriver at the moment.");
+        return browserIs(BrowserType.IPAD);
     }
 
     public static boolean browserIsIPhone() {
-        if (ThreadDriver.getDriver() instanceof RemoteWebDriver) {
-            return isRemoteWebDriverBrowser(BrowserType.IPHONE);
-        }
-        throw new WebDriverExtensionException("Sorry! browserIsIPhone() is only "
-                + "implemented for RemoteWebDriver at the moment.");
+        return browserIs(BrowserType.IPHONE);
     }
 
     public static boolean browserIsInternetExplorer() {
-        if (ThreadDriver.getDriver() instanceof RemoteWebDriver) {
-            return isRemoteWebDriverBrowser(BrowserType.IE);
-        } else if (ThreadDriver.getDriver() instanceof InternetExplorerDriver) {
-            return true;
-        }
-        return false;
+        return browserIs(BrowserType.IE) || browserIs(BrowserType.IEXPLORE);
     }
 
     public static boolean browserIsOpera() {
-        if (ThreadDriver.getDriver() instanceof RemoteWebDriver) {
-            return isRemoteWebDriverBrowser(BrowserType.OPERA);
-        }
-        throw new WebDriverExtensionException("Sorry! browserIsOpera() is only "
-                + "implemented for RemoteWebDriver at the moment.");
+        return browserIs(BrowserType.OPERA);
     }
 
     public static boolean browserIsPhantomJS() {
-        if (ThreadDriver.getDriver() instanceof RemoteWebDriver) {
-            return isRemoteWebDriverBrowser(BrowserType.PHANTOMJS);
-        }
-        throw new WebDriverExtensionException("Sorry! browserIsPhantomJS() is only "
-                + "implemented for RemoteWebDriver at the moment.");
+        return browserIs(BrowserType.PHANTOMJS);
     }
 
     public static boolean browserIsSafari() {
-        if (ThreadDriver.getDriver() instanceof RemoteWebDriver) {
-            return isRemoteWebDriverBrowser(BrowserType.SAFARI);
-        } else if (ThreadDriver.getDriver() instanceof SafariDriver) {
-            return true;
-        }
-        return false;
-    }
-
-    /* Browser Utils */
-    private static boolean isRemoteWebDriverBrowser(String browserName) {
-        String currentBrowserName = ((RemoteWebDriver) ThreadDriver.getDriver()).getCapabilities().getBrowserName();
-        return StringUtils.equalsIgnoreCase(browserName, currentBrowserName);
+        return browserIs(BrowserType.SAFARI);
     }
 
     /* Browser Version */
     public static String browserVersion() {
-        if (ThreadDriver.getDriver() instanceof RemoteWebDriver) {
-            return ((RemoteWebDriver) ThreadDriver.getDriver()).getCapabilities().getVersion();
-        }
-        throw new WebDriverExtensionException("Sorry! browserVersion() is only "
-                + "implemented for RemoteWebDriver at the moment.");
+        return ((HasCapabilities) ThreadDriver.getDriver()).getCapabilities().getVersion();
     }
 
     /* Platform */
     public static Platform platform() {
-        if (ThreadDriver.getDriver() instanceof RemoteWebDriver) {
-            return ((RemoteWebDriver) ThreadDriver.getDriver()).getCapabilities().getPlatform();
-        }
-        throw new WebDriverExtensionException("Sorry! platform() is only "
-                + "implemented for RemoteWebDriver at the moment.");
+        return ((HasCapabilities) ThreadDriver.getDriver()).getCapabilities().getPlatform();
     }
 
     /* Execute Javascript */
