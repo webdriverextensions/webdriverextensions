770ef31d463bf3dde5dedc01994a6d048e9e61c5
Anders Steiner <anders.steiner@gmail.com>
2014-04-24 21:49:59 +0200

    Removed overflow of constructors

--- src/main/java/com/github/webdriverextensions/ThreadDriver.java
+++ src/main/java/com/github/webdriverextensions/ThreadDriver.java
@@ -1,11 +1,7 @@
 package com.github.webdriverextensions;
 
-import java.net.URL;
 import com.github.webdriverextensions.internal.WebDriverExtensionException;
-import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class ThreadDriver {
 
@@ -26,35 +22,4 @@ public class ThreadDriver {
         threadDriver.set(driver);
     }
 
-    public static void setDriver(DesiredCapabilities desiredCapabilities) {
-        threadDriver.set(new RemoteWebDriver(desiredCapabilities));
-    }
-
-    public static void setDriver(URL remoteAddress, DesiredCapabilities desiredCapabilities) {
-        threadDriver.set(new RemoteWebDriver(remoteAddress, desiredCapabilities));
-    }
-
-    public static void setDriver(String browserName) {
-        threadDriver.set(new RemoteWebDriver(new DesiredCapabilities(browserName, "", Platform.ANY)));
-    }
-
-    public static void setDriver(URL remoteAddress, String browserName) {
-        threadDriver.set(new RemoteWebDriver(remoteAddress, new DesiredCapabilities(browserName, "", Platform.ANY)));
-    }
-
-    public static void setDriver(String browserName, String browserVersion) {
-        threadDriver.set(new RemoteWebDriver(new DesiredCapabilities(browserName, browserVersion, Platform.ANY)));
-    }
-
-    public static void setDriver(URL remoteAddress, String browserName, String browserVersion) {
-        threadDriver.set(new RemoteWebDriver(remoteAddress, new DesiredCapabilities(browserName, browserVersion, Platform.ANY)));
-    }
-
-    public static void setDriver(String browserName, String browserVersion, Platform platform) {
-        threadDriver.set(new RemoteWebDriver(new DesiredCapabilities(browserName, browserVersion, platform)));
-    }
-
-    public static void setDriver(URL remoteAddress, String browserName, String browserVersion, Platform platform) {
-        threadDriver.set(new RemoteWebDriver(remoteAddress, new DesiredCapabilities(browserName, browserVersion, platform)));
-    }
 }
