b3fbcb76caabf6ccb3fe63f3499889254dae092e
Anders Steiner <anders.steiner@gmail.com>
2014-06-12 23:05:32 +0200

    Updated WebDriverRunner test

--- src/test/java/com/github/webdriverextensions/junitrunner/WebDriverRunnerTest.java
+++ src/test/java/com/github/webdriverextensions/junitrunner/WebDriverRunnerTest.java
@@ -1,7 +1,6 @@
 package com.github.webdriverextensions.junitrunner;
 
 import static com.github.webdriverextensions.Bot.*;
-import static com.github.webdriverextensions.WebDriverExtensionsContext.getDriver;
 import com.github.webdriverextensions.junitrunner.annotations.Browsers;
 import com.github.webdriverextensions.junitrunner.annotations.Chrome;
 import com.github.webdriverextensions.junitrunner.annotations.Firefox;
@@ -10,15 +9,13 @@ import com.github.webdriverextensions.junitrunner.annotations.InternetExplorer;
 import com.github.webdriverextensions.junitrunner.annotations.Safari;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
 @RunWith(WebDriverRunner.class)
-//@Firefox
-//@Chrome
-//@InternetExplorer
-//@HtmlUnit
-//@Safari
+@Firefox
+@Chrome
+@InternetExplorer
+@HtmlUnit
+@Safari
 @Browsers(chrome = {
     @Chrome(desiredCapabilities = "{chromeOptions: {args: ['start-maximized']}}"),
     @Chrome(desiredCapabilitiesClass = StartChromeMaximized.class)
@@ -28,10 +25,6 @@ public class WebDriverRunnerTest {
     @Test
     public void successfulTest() {
         open("https://github.com");
-        assertCurrentUrlStartsWith("https://github.com");
-        WebElement element = getDriver().findElement(By.name("q"));
-        click(element);
-        debug(element);
-        waitFor(3);
+        assertCurrentUrlContains("github");
     }
 }
