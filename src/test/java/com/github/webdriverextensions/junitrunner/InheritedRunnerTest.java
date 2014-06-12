0ad51adb4d0e9513f63774da7eea8dd12c8f3477
Anders Steiner <anders.steiner@gmail.com>
2014-06-12 23:01:17 +0200

    Updated InheritedRunnerTest

--- src/test/java/com/github/webdriverextensions/junitrunner/InheritedRunnerTest.java
+++ src/test/java/com/github/webdriverextensions/junitrunner/InheritedRunnerTest.java
@@ -1,19 +1,14 @@
 package com.github.webdriverextensions.junitrunner;
 
+import static com.github.webdriverextensions.Bot.assertCurrentUrlContains;
 import static com.github.webdriverextensions.Bot.open;
 import org.junit.Test;
-import com.github.webdriverextensions.WebDriverExtensionsContext;
-import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class InheritedRunnerTest extends InheritedRunner {
 
     @Test
     public void test() {
-        System.out.println("browserName = " + ((RemoteWebDriver) WebDriverExtensionsContext.getDriver()).getCapabilities().getBrowserName());
-        System.out.println("browserVersion = " + ((RemoteWebDriver) WebDriverExtensionsContext.getDriver()).getCapabilities().getVersion());
-        System.out.println("platformName = " + ((RemoteWebDriver) WebDriverExtensionsContext.getDriver()).getCapabilities().getPlatform().toString());
-
         open("http://www.google.com");
-        System.out.println("Ran InheritedRunner successfully!");
+        assertCurrentUrlContains("google");
     }
 }
