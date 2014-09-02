1ff0d7a62bf55396831aa422e739b7e6e59290eb
Anders Steiner <anders.steiner@gmail.com>
2014-09-02 17:00:17 +0200

    Added is not methods to is browser / is version / is platform bot methods

--- src/main/java/com/github/webdriverextensions/internal/ExperimentalBot.java
+++ src/main/java/com/github/webdriverextensions/internal/ExperimentalBot.java
@@ -19,44 +19,88 @@ public class ExperimentalBot {
         return StringUtils.equalsIgnoreCase(browser(), browserName);
     }
 
+    public static boolean browserIsNot(String browserName) {
+        return !browserIs(browserName);
+    }
+
     public static boolean browserIsAndroid() {
         return browserIs(BrowserType.ANDROID);
+    }
+
+    public static boolean browserIsNotAndroid() {
+        return !browserIsAndroid();
     }
 
     public static boolean browserIsChrome() {
         return browserIs(BrowserType.CHROME) || browserIs(BrowserType.GOOGLECHROME);
     }
 
+    public static boolean browserIsNotChrome() {
+        return !browserIsChrome();
+    }
+
     public static boolean browserIsFirefox() {
         return browserIs(BrowserType.FIREFOX);
+    }
+
+    public static boolean browserIsNotFirefox() {
+        return !browserIsFirefox();
     }
 
     public static boolean browserIsHtmlUnit() {
         return browserIs(BrowserType.HTMLUNIT);
     }
 
+    public static boolean browserIsNotHtmlUnit() {
+        return !browserIsHtmlUnit();
+    }
+
     public static boolean browserIsIPad() {
         return browserIs(BrowserType.IPAD);
+    }
+
+    public static boolean browserIsNotIPad() {
+        return !browserIsIPad();
     }
 
     public static boolean browserIsIPhone() {
         return browserIs(BrowserType.IPHONE);
     }
 
+    public static boolean browserIsNotIPhone() {
+        return !browserIsIPhone();
+    }
+
     public static boolean browserIsInternetExplorer() {
         return browserIs(BrowserType.IE) || browserIs(BrowserType.IEXPLORE);
+    }
+
+    public static boolean browserIsNotInternetExplorer() {
+        return !browserIsInternetExplorer();
     }
 
     public static boolean browserIsOpera() {
         return browserIs(BrowserType.OPERA);
     }
 
+    public static boolean browserIsNotOpera() {
+        return !browserIsOpera();
+    }
+
     public static boolean browserIsPhantomJS() {
         return browserIs(BrowserType.PHANTOMJS);
     }
 
+    public static boolean browserIsNotPhantomJS() {
+        return !browserIsPhantomJS();
+    }
+
     public static boolean browserIsSafari() {
         return browserIs(BrowserType.SAFARI);
+    }
+
+    public static boolean browserIsNotSafari() {
+        return !browserIsSafari();
     }
 
     /* Version */
@@ -68,6 +112,10 @@ public class ExperimentalBot {
         return StringUtils.equalsIgnoreCase(version(), version);
     }
 
+    public static boolean versionIsNot(String version) {
+        return !versionIs(version);
+    }
+
     /* Platform */
     public static Platform platform() {
         return ((HasCapabilities) WebDriverExtensionsContext.getDriver()).getCapabilities().getPlatform();
@@ -77,32 +125,72 @@ public class ExperimentalBot {
         return platform().is(platform);
     }
 
-    public static boolean platformIsWindowsXP() {
-        return platformIs(Platform.XP);
-    }
-
-    public static boolean platformIsWindowsVista() {
-        return platformIs(Platform.VISTA);
-    }
-
-    public static boolean platformIsWindows8() {
-        return platformIs(Platform.WIN8);
-    }
-
-    public static boolean platformIsUnix() {
-        return platformIs(Platform.UNIX);
-    }
-
-    public static boolean platformIsLinux() {
-        return platformIs(Platform.LINUX);
+    public static boolean platformIsNot(Platform platform) {
+        return !platformIs(platform);
     }
 
     public static boolean platformIsAndroid() {
         return platformIs(Platform.ANDROID);
     }
 
+    public static boolean platformIsNotAndroid() {
+        return !platformIsAndroid();
+    }
+
+    public static boolean platformIsLinux() {
+        return platformIs(Platform.LINUX);
+    }
+
+    public static boolean platformIsNotLinux() {
+        return !platformIsLinux();
+    }
+
     public static boolean platformIsMac() {
         return platformIs(Platform.MAC);
+    }
+
+    public static boolean platformIsNotMac() {
+        return !platformIsMac();
+    }
+
+    public static boolean platformIsUnix() {
+        return platformIs(Platform.UNIX);
+    }
+
+    public static boolean platformIsNotUnix() {
+        return !platformIsUnix();
+    }
+
+    public static boolean platformIsWindows() {
+        return platformIs(Platform.WINDOWS);
+    }
+
+    public static boolean platformIsNotWindows() {
+        return !platformIsWindows();
+    }
+
+    public static boolean platformIsWindows8() {
+        return platformIs(Platform.WIN8);
+    }
+
+    public static boolean platformIsNotWindows8() {
+        return !platformIsWindows8();
+    }
+
+    public static boolean platformIsWindowsVista() {
+        return platformIs(Platform.VISTA);
+    }
+
+    public static boolean platformIsNotWindowsVista() {
+        return !platformIsWindowsVista();
+    }
+
+    public static boolean platformIsWindowsXP() {
+        return platformIs(Platform.XP);
+    }
+
+    public static boolean platformIsNotWindowsXP() {
+        return !platformIsWindowsXP();
     }
 
     /* Execute Javascript */
